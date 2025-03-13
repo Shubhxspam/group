eafcdae5ce5647736d76ae5594e12a16586d58ed
𝐌я.𝐏αѕѕєяву <164321506+Shubhxspam@users.noreply.github.com>
2025-03-13 18:24:56 +0530

    Update google.py

--- MukeshRobot/modules/google.py
+++ MukeshRobot/modules/google.py
@@ -20,103 +20,35 @@ useragent = "Mozilla/5.0 (Linux; Android 11; SM-M017F Build/PPR1.180610.011; wv)
 opener.addheaders = [("User-agent", useragent)]
 
 
-import os
-import requests
-from pyrogram import Client, filters
-
-# Google API & CX ID from Environment Variables
-GOOGLE_API_KEY = os.getenv("AIzaSyCVgY0zwTEZDXY2wkOrgvARNTcjaU5A12c")
-GOOGLE_CX = os.getenv("6063aa3400fcf46cc")
-
-@Client.on_message(filters.command("google") & filters.private)
-async def google_search(client, message):
-    if not GOOGLE_API_KEY or not GOOGLE_CX:
-        await message.reply_text("`Google API key ya CX ID set nahi hai!`")
+@register(pattern="^/google (.*)")
+async def _(event):
+    if event.fwd_from:
         return
 
-    query = message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else None
-    if not query:
-        await message.reply_text("`Kya search karna hai likho!`")
-        return
-
-    await message.reply_text("🔍 **Searching Google...**")
-
-    # Google Search API request
-    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_API_KEY}&cx={GOOGLE_CX}"
-    
+    webevent = await event.reply("Searching...")
+    match = event.pattern_match.group(1)
+    page = re.findall(r"page=\d+", match)
     try:
-        response = requests.get(url)
-        data = response.json()
-
-        if "items" not in data:
-            await message.reply_text("`Koi results nahi mile ya API limit exceed ho gayi.`")
-            return
-        
-        results = data["items"][:5]  # Top 5 results
-        reply_text = f"🔎 **Google Search Results for:** `{query}`\n\n"
-
-        for idx, item in enumerate(results, start=1):
-            title = item.get("title", "No Title")
-            link = item.get("link", "No Link")
-            reply_text += f"**{idx}.** [{title}]({link})\n"
-
-        await message.reply_text(reply_text, disable_web_page_preview=True)
-    
-    except Exception as e:
-        await message.reply_text(f"`Google search me error aaya:` {str(e)}")
-            try:
-
-    
-
-
-     @register(pattern="^/img (.*)")
-     async def img_sampler(event):
-     if event.fwd_from:
-         return
-
-    query = event.pattern_match.group(1)
-    jit = f'"{query}"'
-    downloader.download(
-        jit,
-        limit=4,
-        output_dir="store",
-        adult_filter_off=False,
-        force_replace=False,
-        timeout=60,
-    )
-    os.chdir(f'./store/"{query}"')
-    types = ("*.png", "*.jpeg", "*.jpg")  # the tuple of file types
-    files_grabbed = []
-    for files in types:
-        files_grabbed.extend(glob.glob(files))
-    await tbot.send_file(event.chat_id, files_grabbed, reply_to=event.id)
-    os.chdir("/app")
-    os.system("rm -rf store")
-
-
-opener = urllib.request.build_opener()
-useragent = "Mozilla/5.0 (Linux; Android 11; SM-M017F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
-opener.addheaders = [("User-agent", useragent)]
-
-
-@register(pattern=r"^/reverse|^/pp|^/grs(?: |$)(\d*)")
-async def okgoogle(img):
-    """For .reverse command, Google search images and stickers."""
-    if os.path.isfile("okgoogle.png"):
-        os.remove("okgoogle.png")
-
-    message = await img.get_reply_message()
-    if message and message.media:
-        photo = io.BytesIO()
-        await tbot.download_media(message, photo)
-    else:
-        await img.reply("`Reply to photo or sticker fu*ker`")
-        return
-
-    if photo:
-        dev = await img.reply("`Processing...`")
+        page = page[0]
+        page = page.replace("page=", "")
+        match = match.replace("page=" + page[0], "")
+    except IndexError:
+        page = 1
+    search_args = (str(match), int(page))
+    gsearch = GoogleSearch()
+    gresults = await gsearch.async_search(*search_args)
+    msg = ""
+    for i in range(len(gresults["links"])):
         try:
-
+            title = gresults["titles"][i]
+            link = gresults["links"][i]
+            desc = gresults["descriptions"][i]
+            msg += f"❍[{title}]({link})\n**{desc}**\n\n"
+        except IndexError:
+            break
+    await webevent.edit(
+        "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
+    )
 
 
 @register(pattern="^/img (.*)")
@@ -175,15 +107,6 @@ async def okgoogle(img):
         image.close()
         # https://stackoverflow.com/questions/23270175/google-reverse-image-search-using-post-request#28792943
         searchUrl = "https://www.google.com/searchbyimage/upload"
-            image = Image.open(photo)
-        except OSError:
-            await dev.edit("`Unsupported sexuality, most likely.`")
-            return
-        name = "okgoogle.png"
-        image.save(name, "PNG")
-        image.close()
-        # https://stackoverflow.com/questions/23270175/google-reverse-image-search-using-post-request#28792943
-        searchUrl = "https://www.google.com/searchbyimage/upload"
         multipart = {"encoded_image": (name, open(name, "rb")), "image_content": ""}
         response = requests.post(searchUrl, files=multipart, allow_redirects=False)
         fetchUrl = response.headers["Location"]
@@ -193,7 +116,85 @@ async def okgoogle(img):
                 "`Image successfully uploaded to Google. Maybe.`"
                 "\n`Parsing source now. Maybe.`"
             )
+        else:
+            await dev.edit("`Google told me to fu*k off.`")
+            return
 
+        os.remove(name)
+        match = await ParseSauce(fetchUrl + "&preferences?hl=en&fg=1#languages")
+        guess = match["best_guess"]
+        imgspage = match["similar_images"]
+
+        if guess and imgspage:
+            await dev.edit(f"[{guess}]({fetchUrl})\n\n`Looking for this Image...`")
+        else:
+            await dev.edit("`Can't find this piece of shit.`")
+            return
+
+        if img.pattern_match.group(1):
+            lim = img.pattern_match.group(1)
+        else:
+            lim = 3
+        images = await scam(match, lim)
+        yeet = []
+        for i in images:
+            k = requests.get(i)
+            yeet.append(k.content)
+        try:
+            await tbot.send_file(
+                entity=await tbot.get_input_entity(img.chat_id),
+                file=yeet,
+                reply_to=img,
+            )
+        except TypeError:
+            pass
+        await dev.edit(
+            f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})"
+        )
+
+
+async def ParseSauce(googleurl):
+    """Parse/Scrape the HTML code for the info we want."""
+
+    source = opener.open(googleurl).read()
+    soup = BeautifulSoup(source, "html.parser")
+
+    results = {"similar_images": "", "best_guess": ""}
+
+    try:
+        for similar_image in soup.findAll("input", {"class": "gLFyf"}):
+            url = "https://www.google.com/search?tbm=isch&q=" + urllib.parse.quote_plus(
+                similar_image.get("value")
+            )
+            results["similar_images"] = url
+    except BaseException:
+        pass
+
+    for best_guess in soup.findAll("div", attrs={"class": "r5a77d"}):
+        results["best_guess"] = best_guess.get_text()
+
+    return results
+
+
+async def scam(results, lim):
+
+    single = opener.open(results["similar_images"]).read()
+    decoded = single.decode("utf-8")
+
+    imglinks = []
+    counter = 0
+
+    pattern = r"^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$"
+    oboi = re.findall(pattern, decoded, re.I | re.M)
+
+    for imglink in oboi:
+        counter += 1
+        if counter < int(lim):
+            imglinks.append(imglink)
+        else:
+            break
+
+    return imglinks
 
 
 @register(pattern="^/app (.*)")
