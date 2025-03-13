c35af54c5f50d8be55b9439011ba025794bfd05d
𝐌я.𝐏αѕѕєяву <164321506+Shubhxspam@users.noreply.github.com>
2025-03-13 17:38:11 +0530

    Update google.py

--- MukeshRobot/modules/google.py
+++ MukeshRobot/modules/google.py
@@ -116,35 +116,7 @@ async def okgoogle(img):
     if photo:
         dev = await img.reply("`Processing...`")
         try:
-@register(pattern="^/google (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
 
-    webevent = await event.reply("Searching...")
-    match = event.pattern_match.group(1)
-    page = re.findall(r"page=\d+", match)
-    try:
-        page = page[0]
-        page = page.replace("page=", "")
-        match = match.replace("page=" + page[0], "")
-    except IndexError:
-        page = 1
-    search_args = (str(match), int(page))
-    gsearch = GoogleSearch()
-    gresults = await gsearch.async_search(*search_args)
-    msg = ""
-    for i in range(len(gresults["links"])):
-        try:
-            title = gresults["titles"][i]
-            link = gresults["links"][i]
-            desc = gresults["descriptions"][i]
-            msg += f"❍[{title}]({link})\n**{desc}**\n\n"
-        except IndexError:
-            break
-    await webevent.edit(
-        "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
-    )
 
 
 @register(pattern="^/img (.*)")
