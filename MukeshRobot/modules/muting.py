ec4a0393bc31f808d1938b5b467cd1402a3cdab7
𝐌я.𝐏αѕѕєяву <164321506+Shubhxspam@users.noreply.github.com>
2025-03-14 16:20:37 +0530

    Update muting.py

--- MukeshRobot/modules/muting.py
+++ MukeshRobot/modules/muting.py
@@ -285,10 +285,10 @@ __help__ = """
  ❍ /unmute <ᴜsᴇʀʜᴀɴᴅʟᴇ>*:* ᴜɴᴍᴜᴛᴇs ᴀ ᴜsᴇʀ. ᴄᴀɴ ᴀʟsᴏ ʙᴇ ᴜsᴇᴅ ᴀs ᴀ ʀᴇᴘʟʏ, ᴍᴜᴛɪɴɢ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴜsᴇʀ.
  ❍ /dmute <ᴜsᴇʀʜᴀɴᴅʟᴇ>*:* sɪʟᴇɴᴄᴇs ᴀ ᴜsᴇʀ. ᴄᴀɴ ᴀʟsᴏ ʙᴇ ᴜsᴇᴅ ᴀs ᴀ ʀᴇᴘʟʏ, ᴍᴜᴛɪɴɢ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴜsᴇʀ.
 """
-DMUTE_HANDLER = CommandHandler("dmute", dmute, run_async=True)
-MUTE_HANDLER = CommandHandler("mute", mute, run_async=True)
-UNMUTE_HANDLER = CommandHandler("unmute", unmute, run_async=True)
-TEMPMUTE_HANDLER = CommandHandler(["tmute", "tempmute"], temp_mute, run_async=True)
+DMUTE_HANDLER = CommandHandler("dmuteh", dmute, run_async=True)
+MUTE_HANDLER = CommandHandler("muteh", mute, run_async=True)
+UNMUTE_HANDLER = CommandHandler("unmute"h, unmute, run_async=True)
+TEMPMUTE_HANDLER = CommandHandler(["tmuteh", "tempmuteh"], temp_mute, run_async=True)
 dispatcher.add_handler(DMUTE_HANDLER)
 dispatcher.add_handler(MUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_HANDLER)
