2f9d8017ea6dd0f7995cf66b1aa2d642b7f4f0b4
𝐌я.𝐏αѕѕєяву <164321506+Shubhxspam@users.noreply.github.com>
2025-03-15 13:59:55 +0530

    Update notes.py

--- MukeshRobot/modules/notes.py
+++ MukeshRobot/modules/notes.py
@@ -514,10 +514,10 @@ SLASH_GET_HANDLER = MessageHandler(Filters.regex(r"^/\d+$"), slash_get, run_asyn
 SAVE_HANDLER = CommandHandler("save", save, run_async=True)
 DELETE_HANDLER = CommandHandler("clear", clear, run_async=True)
 LIST_HANDLER = DisableAbleCommandHandler(
-    ["notes", "saved"], list_notes, admin_ok=True, run_async=True
+    ["notesh", "saved"], list_notes, admin_ok=True, run_async=True
 )
 CLEARALL = DisableAbleCommandHandler("removeallnotes", clearall, run_async=True)
-CLEARALL_BTN = CallbackQueryHandler(clearall_btn, pattern=r"notes_.*", run_async=True)
+CLEARALL_BTN = CallbackQueryHandler(clearall_btn, pattern=r"notesh_.*", run_async=True)
 
 dispatcher.add_handler(GET_HANDLER)
 dispatcher.add_handler(SAVE_HANDLER)
