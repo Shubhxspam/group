be7299a9dfd62cb390e54072dccd1ba7469714c7
𝐌я.𝐏αѕѕєяву <164321506+Shubhxspam@users.noreply.github.com>
2025-03-15 17:32:59 +0530

    Update google.py

--- MukeshRobot/modules/google.py
+++ MukeshRobot/modules/google.py
@@ -16,7 +16,7 @@ useragent = "Mozilla/5.0 (Linux; Android 11; SM-M017F Build/PPR1.180610.011; wv)
 opener.addheaders = [("User-agent", useragent)]
 
 
-@register(pattern="^/google (.*)")
+@register(pattern="^/googleh (.*)")
 async def google_search(event):
     if event.fwd_from:
         return
